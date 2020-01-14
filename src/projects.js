2a0deecc8eb9f82ee5aba751f301acb3c515a3b2
Paul Bailey <paul.m.bailey@gmail.com>
2020-01-14 10:58:49 -0600

    updated projects

--- src/projects.js
+++ src/projects.js
@@ -2,17 +2,6 @@ import Vue from 'vue';
 
 var PROJECTS = [
   {
-    title: "Neutron64",
-    subtitle: "The honey badger of code editors.",
-    icon: "laptop",
-    url: "https://www.neutron64.com/",
-    desc: "Neutron64 is a web based code editor build as a progressive\
-    web app. Neutron is simple yet powerful and supports editing files\
-    locally, remotely, and through cloud storage systems like\
-    Google Drive. Neutron turns your web browser into a development \
-    environment."
-  },
-  {
     title: "CanyonLake.app",
     subtitle: "Your guide to fun in Canyon Lake, Texas",
     icon: "waves",
@@ -22,13 +11,20 @@ var PROJECTS = [
     Canyon Lake, Texas area."
   },
   {
-    title: "Bible Fish",
-    subtitle: "The quickest and simplest way to read the Bible on any device.",
-    icon: "local_library",
-    url: "https://www.bible.fish/",
-    desc: "A free Progressive Web App Bible. Bible.Fish is a Bible app that \
-    allows quick access to scriptures with just your web browser. You can also \
-    use Bible.Fish offline without any internet on Chrome, Firefox, and Opera."
+    title: "FlipRank App",
+    subtitle: "Helping you organize your small business.",
+    icon: "phone_iphone",
+    url: "https://www.fliprankapp.com/",
+    desc: "FlipRank helps you organize your rank based small businesses such as \
+    Young Living based businesses."
+  },
+  {
+    title: "PodcastBible.app",
+    subtitle: "Create a custom Bible Podcast.",
+    icon: "surround_sound",
+    url: "https://www.podcastbible.app/",
+    desc: "The Podcast Bible lets you create a custom reading plan tailored to \
+    your needs via a custom podcast feed."
   }
 ];
 
